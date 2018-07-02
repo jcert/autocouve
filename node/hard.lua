dc1cb330f9ce891b5ed2c46a6efe9ceeb0f78a4a
mlbj <mrbrjr@gmail.com>
2018-07-02 13:32:27 -0300

    correcao dos retornos

--- node/hard.lua
+++ node/hard.lua
@@ -9,13 +9,13 @@ funcao_cod["HELP"]="6"
 
 pino={}
 pino["bomba1"]="02" 		-- digital
-pino["bomba2"]="04" 		-- digital
-pino["bomba3"]="07" 		-- digital
+pino["bomba2"]="07" 		-- digital
+pino["bomba3"]="04" 		-- digital
 pino["bomba4"]="08" 		-- digital
-pino["i_led1"]="03"		-- pwm
+pino["i_led1"]="09"		-- pwm
 pino["i_led2"]="05"		-- pwm
 pino["i_led3"]="06"		-- pwm
-pino["i_led4"]="09"		-- pwm
+pino["i_led4"]="03"		-- pwm
 pino["coolers"]="12"		-- digital
 pino["t_led1"]="00"		-- analogica
 pino["t_led2"]="01"		-- analogica
@@ -36,15 +36,15 @@ function escreve_bomba(vaso_id,tempo)
 	com(fn,p)
 	tmr.delay(tempo*1000000)
 	p=bomba_pino.."000"
-	com(fn,p)
+	return com(fn,p)
 end
 
 function le_bomba(vaso_id)
 	bomba_index="bomba"..vaso_id
 	bomba_pino=pino[bomba_index]
-	fn=funcao_cod["DI"]
+	fn=funcao_cod["READ"]
 	p=bomba_pino
-	com(fn,p)
+	return com(fn,p)
 end
 
 function le_umid_vaso(vaso_id)
@@ -52,23 +52,24 @@ function le_umid_vaso(vaso_id)
 	umid_vaso_pino=pino[umid_vaso_index]
 	fn=funcao_cod["AI"]
 	p=umid_vaso_pino
-	com(fn,p)
+	return com(fn,p)
 end
 
 function escreve_i_led(led_id,valor)
 	i_led_index="i_led"..led_id
 	i_led_pino=pino[i_led_index]
 	fn=funcao_cod["PWM"]
-	p=i_led_pino..valor
-	com(fn,p)
+	valor_formatado=string.format("%03d",valor)
+	p=i_led_pino..valor_formatado
+	return com(fn,p)
 end
 
 function le_i_led(led_id)
-	i_led_index="i_led"..i_led_id
+	i_led_index="i_led"..led_id
 	i_led_pino=pino[i_led_index]
-	fn=funcao_cod["AI"]
+	fn=funcao_cod["READ"]
 	p=i_led_pino
-	com(fn,p)
+	return com(fn,p)
 end
 
 function le_t_led(led_id)
@@ -76,7 +77,7 @@ function le_t_led(led_id)
 	t_led_pino=pino[t_led_index]
 	fn=funcao_cod["AI"]
 	p=t_led_pino
-	com(fn,p)
+	return com(fn,p)
 end
 
 function escreve_coolers(valor)
@@ -84,14 +85,14 @@ function escreve_coolers(valor)
 	fn=funcao_cod["DO"]
 	valor_formatado=string.format("%03d",valor)
 	p=coolers_pino..valor_formatado
-	com(fn,p)
+	return com(fn,p)
 end
 
 function le_coolers()
 	coolers_pino=pino["coolers"]
-	fn=funcao_cod["DI"]
+	fn=funcao_cod["READ"]
 	p=coolers_pino
-	com(fn,p)
+	return com(fn,p)
 end
 
 function le_nivel_reserv(reserv_id)
@@ -99,10 +100,25 @@ function le_nivel_reserv(reserv_id)
 	nivel_reserv_pino=pino[nivel_reserv_index]
 	fn=funcao_cod["DI"]
 	p=nivel_reserv_pino
-	com(fn,p)
+	return com(fn,p)
 end
 
 
-
-
+function init_hard()
+	escreve_coolers(1)
+	print("coolers :"..le_coolers())
+	
+	escreve_i_led(1,255)
+	tmr.delay(1000000*2)
+	escreve_i_led(2,255)
+	tmr.delay(1000000*2)
+	escreve_i_led(3,255)
+	tmr.delay(1000000*2)
+	escreve_i_led(4,255)
+	
+	print("i_led1: "..le_i_led(1))
+	print("i_led2: "..le_i_led(2))
+	print("i_led3: "..le_i_led(3))
+	print("i_led4: "..le_i_led(4))
+end
 
