e92bee4228620b0d7a6da8bcb772a393a859cc8c
mauro <mrbrjr@gmail.com>
2019-01-04 11:20:58 -0200

    correcoes no main_loop

--- node/main_loop.lua
+++ node/main_loop.lua
@@ -15,17 +15,17 @@ function main_loop()
     if not last_min_ilu==min then    
         -- acionamento dos coolers
         if han_luz[min]==0 then
-            com("3","12000")    -- 3: d0, 12: coolers, 000: desligado               
+            com(nano1_addr,"3","12000")    -- 3: d0, 12: coolers, 000: desligado               
         else
-            com("3","12001")    -- 3: d0, 12: coolers, 001: ligado
+            com(nano1_addr,"3","12001")    -- 3: d0, 12: coolers, 001: ligado
         end    
 
         -- acionamento dos leds
         han_luz_formatado=string.format("%03d",han_luz[min])
-        com("1","09"..han_luz_formatado)  -- 1: pwm, 09: i_led1    
-        com("1","05"..han_luz_formatado)  -- 1: pwm, 05: i_led2    
-        com("1","06"..han_luz_formatado)  -- 1: pwm, 06: i_led3    
-        com("1","03"..han_luz_formatado)  -- 1: pwm, 03: i_led4    
+        com(nano1_addr,"1","09"..han_luz_formatado)  -- 1: pwm, 09: i_led1    
+        com(nano1_addr,"1","05"..han_luz_formatado)  -- 1: pwm, 05: i_led2    
+        com(nano1_addr,"1","06"..han_luz_formatado)  -- 1: pwm, 06: i_led3    
+        com(nano1_addr,"1","03"..han_luz_formatado)  -- 1: pwm, 03: i_led4    
 
         -- altera last_min_ilu
         last_min_ilu=min
@@ -35,18 +35,18 @@ function main_loop()
     if not last_hour_irrig==hour then
         if not kbc_bomba[hour]==0 then
             -- liga bombas            
-            com("3","02001")    -- 3: d0, 02: bomba1, 001: ligado
-            com("3","07001")    -- 3: d0, 07: bomba2, 001: ligado
-            com("3","04001")    -- 3: d0, 04: bomba3, 001: ligado
-            com("3","08001")    -- 3: d0, 08: bomba4, 001: ligado
+            com(nano1_addr,"3","02001")    -- 3: d0, 02: bomba1, 001: ligado
+            com(nano1_addr,"3","07001")    -- 3: d0, 07: bomba2, 001: ligado
+            com(nano1_addr,"3","04001")    -- 3: d0, 04: bomba3, 001: ligado
+            com(nano1_addr,"3","08001")    -- 3: d0, 08: bomba4, 001: ligado
 
             -- programa o desligamento das bombas
             timer_bomba=tmr.create()
 	        timer_bomba:register(kbc_bomba[hour]*1000,tmr.ALARM_SINGLE,function()
-                com("3","02000")    -- 3: d0, 02: bomba1, 000: desligado
-                com("3","07000")    -- 3: d0, 07: bomba2, 000: desligado
-                com("3","04000")    -- 3: d0, 04: bomba3, 000: desligado
-                com("3","08000")    -- 3: d0, 08: bomba4, 000: desligado
+                com(nano1_addr,"3","02000")    -- 3: d0, 02: bomba1, 000: desligado
+                com(nano1_addr,"3","07000")    -- 3: d0, 07: bomba2, 000: desligado
+                com(nano1_addr,"3","04000")    -- 3: d0, 04: bomba3, 000: desligado
+                com(nano1_addr,"3","08000")    -- 3: d0, 08: bomba4, 000: desligado
 	        end)
 	        timer_bomba:start()        
         end
