8b466a765b356aa62b60f5b7c14352f58ef18ce7
jcert <jgcertorio@uol.com.br>
2019-01-05 11:27:25 -0200

    fix to wifi

--- node/wifi.lua
+++ node/wifi.lua
@@ -1,8 +1,13 @@
 enduser_setup.start(
   function()
-    print("Connected to wifi as:" .. wifi.sta.getip())
+    print("Connected to wifi as:") -- .. wifi.sta.getip()) se chamar isso aqui, ainda não vai ter ip, daí dá crash por concatenar string .. nil
+    local m = tmr.create()
+    m:alarm(5000, tmr.ALARM_SINGLE, function() sntp.sync(nil, nil, nil, 1) end)
   end,
   function(err, str)
     print("enduser_setup: Err #" .. err .. ": " .. str)
   end
 );
+
+
+
